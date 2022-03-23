bb48d17f6e85ae7b7284570f087b3b9768a47672
Mickey Burks <brks.mck@gmail.com>
2022-03-23 08:46:20 -0500

    Fix lint

--- helpers/index.js
+++ helpers/index.js
@@ -1,7 +1,5 @@
-
 import {promises as __fs} from 'fs';
 import __path from 'path';
-import __util from 'util';
 import __readline from 'readline';
 
 const args = [];
@@ -19,14 +17,11 @@ for (const arg of process.argv.slice(2)) {
   }
 }
 
-export {
-  args,
-  options,
-};
+export {args, options};
 
 export async function readDirVerbose(path) {
   const contents = await __fs.readDir(path, {withFileTypes: true});
-  return contents.map(file => ({
+  return contents.map((file) => ({
     name: file.name,
     type: file.isDirectory() ? 'dir' : 'file',
   }));
@@ -35,10 +30,10 @@ export async function readDirVerbose(path) {
 export async function prompt(question) {
   const rl = __readline.createInterface({
     input: process.stdin,
-    output: process.stdout
+    output: process.stdout,
   });
-  return new Promise(resolve => {
-    rl.question(`${question} [Y/n]`, answer => {
+  return new Promise((resolve) => {
+    rl.question(`${question} [Y/n]`, (answer) => {
       const response = answer === '' || answer.toLowerCase() === 'y';
       rl.close();
       resolve(response);
@@ -50,11 +45,11 @@ export async function recursiveRemove(dirPath) {
   const distFiles = await readDirVerbose(dirPath);
   if (distFiles) {
     await Promise.all(
-      distFiles.map(file => {
+      distFiles.map((file) => {
         if (file.type === 'dir') {
           return recursiveRemove(__path.join(dirPath, file.name));
-        } else { 
-          return __fs.unlink(__path.join(dirPath, file.name))
+        } else {
+          return __fs.unlink(__path.join(dirPath, file.name));
         }
       })
     );
--- main.js
+++ main.js
@@ -6,6 +6,7 @@ import __util from 'util';
 import __cp from 'child_process';
 import * as __helpers from './helpers/index.js';
 
+/* eslint-disable no-unused-vars */
 const pathJoin = __path.join;
 const pathResolve = __path.resolve;
 const pathDirname = __path.dirname;
@@ -22,6 +23,7 @@ const spawn = __util.promisify(__cp.spawn); // usage
 const execFile = __util.promisify(__cp.execFile); // usage
 
 const prompt = __helpers.prompt;
+/* eslint-enable */
 
 // cli args/options
 const args = __helpers.args;
